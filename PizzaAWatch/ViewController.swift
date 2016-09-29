595d70244bd5d874827642cff6e6b39df4c594bd
Ian Arvizu <ianarvizu@la-MacBook-Air-de-Ian.local>
2016-09-29 09:11:31 -0600

    Initial Commit

---
+++ PizzaAWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PizzaAWatch
+//
+//  Created by Ian Arvizu on 29/09/16.
+//  Copyright © 2016 Ian ArvizuBeast9. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
